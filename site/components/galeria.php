052f3011df52aa9d35ba5432c1f5e81bf6116a3c
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-16 11:09:21 -0400

    Atualizações

--- site/components/galeria.php
+++ site/components/galeria.php
@@ -16,22 +16,48 @@
 
           <ul class="portfolio-flters">
             <li data-filter="*" class="filter-active">All</li>
-            <li data-filter=".filter-app">App</li>
+            <!-- <li data-filter=".filter-app">App</li>
             <li data-filter=".filter-product">Product</li>
             <li data-filter=".filter-branding">Branding</li>
-            <li data-filter=".filter-books">Books</li>
+            <li data-filter=".filter-books">Books</li> -->
+            <?php
+            $query = "select * from portifolio_categorias where situacao = '1' order by titulo";
+            $result = mysqli_query($con, $query);
+            while($d = mysqli_fetch_object($result)){
+            ?>
+            <li data-filter=".filter-app<?=$d->codigo?>"><?=$d->titulo?></li>
+            <?php
+            }
+            ?>
           </ul><!-- End Portfolio Filters -->
 
           <div class="row g-0 portfolio-container">
 
-            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
+            <?php
+            $query = "select a.* from portifolio a left join portifolio_categorias b on a.categoria = b.codigo where a.situacao = '1' and b.situacao = '1' order by b.titulo, a.titulo";
+            $result = mysqli_query($con, $query);
+            while($d = mysqli_fetch_object($result)){
+            ?>
+            <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app<?=$d->categoria?>">
+              <img src="assets/img/portfolio/app-1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4><?=$d->titulo?></h4>
+                <a href="assets/img/portfolio/app-1.jpg" title="<?=$d->titulo?>" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+              </div>
+            </div><!-- End Portfolio Item -->
+            <?php
+            }
+            ?>
+
+            <!-- <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
               <img src="assets/img/portfolio/app-1.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>App 1</h4>
                 <a href="assets/img/portfolio/app-1.jpg" title="App 1" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-product">
               <img src="assets/img/portfolio/product-1.jpg" class="img-fluid" alt="">
@@ -40,7 +66,7 @@
                 <a href="assets/img/portfolio/product-1.jpg" title="Product 1" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-branding">
               <img src="assets/img/portfolio/branding-1.jpg" class="img-fluid" alt="">
@@ -49,7 +75,7 @@
                 <a href="assets/img/portfolio/branding-1.jpg" title="Branding 1" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-books">
               <img src="assets/img/portfolio/books-1.jpg" class="img-fluid" alt="">
@@ -58,7 +84,7 @@
                 <a href="assets/img/portfolio/books-1.jpg" title="Branding 1" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
               <img src="assets/img/portfolio/app-2.jpg" class="img-fluid" alt="">
@@ -67,7 +93,7 @@
                 <a href="assets/img/portfolio/app-2.jpg" title="App 2" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-product">
               <img src="assets/img/portfolio/product-2.jpg" class="img-fluid" alt="">
@@ -76,7 +102,7 @@
                 <a href="assets/img/portfolio/product-2.jpg" title="Product 2" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-branding">
               <img src="assets/img/portfolio/branding-2.jpg" class="img-fluid" alt="">
@@ -85,7 +111,7 @@
                 <a href="assets/img/portfolio/branding-2.jpg" title="Branding 2" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-books">
               <img src="assets/img/portfolio/books-2.jpg" class="img-fluid" alt="">
@@ -94,7 +120,7 @@
                 <a href="assets/img/portfolio/books-2.jpg" title="Branding 2" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app">
               <img src="assets/img/portfolio/app-3.jpg" class="img-fluid" alt="">
@@ -103,7 +129,7 @@
                 <a href="assets/img/portfolio/app-3.jpg" title="App 3" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-product">
               <img src="assets/img/portfolio/product-3.jpg" class="img-fluid" alt="">
@@ -112,7 +138,7 @@
                 <a href="assets/img/portfolio/product-3.jpg" title="Product 3" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-branding">
               <img src="assets/img/portfolio/branding-3.jpg" class="img-fluid" alt="">
@@ -121,7 +147,7 @@
                 <a href="assets/img/portfolio/branding-3.jpg" title="Branding 2" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
 
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-books">
               <img src="assets/img/portfolio/books-3.jpg" class="img-fluid" alt="">
@@ -130,7 +156,7 @@
                 <a href="assets/img/portfolio/books-3.jpg" title="Branding 3" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div> -->
 
           </div><!-- End Portfolio Container -->
 
