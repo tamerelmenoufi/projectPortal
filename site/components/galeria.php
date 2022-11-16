2ee8bcad9ab7140cefddc43d121c0bbec6a5ec40
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-16 11:14:41 -0400

    Atualizações

--- site/components/galeria.php
+++ site/components/galeria.php
@@ -39,10 +39,10 @@
             while($d = mysqli_fetch_object($result)){
             ?>
             <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item filter-app<?=$d->categoria?>">
-              <img src="assets/img/portfolio/app-1.jpg" class="img-fluid" alt="">
+              <img src="<?=$localPainel?>src/volume/portifolio/<?=$d->imagem?>" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4><?=$d->titulo?></h4>
-                <a href="assets/img/portfolio/app-1.jpg" title="<?=$d->titulo?>" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                <a href="<?=$localPainel?>src/volume/portifolio/<?=$d->imagem?>" title="<?=$d->titulo?>" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div><!-- End Portfolio Item -->
