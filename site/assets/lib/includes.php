cfa2653d6db736eae027f6c72b269b95c99fc4b0
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-16 10:08:28 -0400

    Atualizações

--- site/assets/lib/includes.php
+++ site/assets/lib/includes.php
@@ -4,5 +4,5 @@
     $con = AppConnect('portal');
     include("classes.php");
 
-    $localPainel = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/painel/";
-    $localSite = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/site/";
+    $localPainel = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/portal/painel/";
+    $localSite = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/portal/site/";
