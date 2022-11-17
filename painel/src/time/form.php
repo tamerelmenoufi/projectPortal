1a0e44dfab113d2ef6d5a6f9e5fb5695740c11f1
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-16 21:39:49 -0400

    Atualizações

--- painel/src/time/form.php
+++ painel/src/time/form.php
@@ -115,9 +115,9 @@
       ?>
       <div class="form-floating mb-3">
         <div class="input-group mb-3">
-          <div class="input-group-text">
+          <!-- <div class="input-group-text">
             <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-          </div>
+          </div> -->
           <div class="input-group-text">
             <?=$url?>
           </div>
