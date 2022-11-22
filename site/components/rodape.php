61fed1ede0347ae450778fda074dcc600f9098c9
livia128924 <livia128924@gmail.com>
2022-11-22 20:35:19 -0300

    atualizando

--- site/components/rodape.php
+++ site/components/rodape.php
@@ -23,7 +23,7 @@
             <!-- You can delete the links only if you purchased the pro version. -->
             <!-- Licensing information: https://bootstrapmade.com/license/ -->
             <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
-           Desenvolvido por <a style="color:#fff" href="https://mohatron.com.br/"><strong>Mohatron Solucções em TI.</strong></a>
+           Desenvolvido por <a style="color:#fff" href="https://mohatron.com.br/"><strong>Mohatron Soluções em TI.</strong></a>
           </div>
         </div>
 
