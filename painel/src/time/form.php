1b0e8d4a47d4c9babafdb2a9def793034f268e61
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-22 14:25:12 -0400

    Atualizações

--- painel/src/time/form.php
+++ painel/src/time/form.php
@@ -144,8 +144,6 @@
       </div>
 
 
-
-
       <button type="submit" data-bs-dismiss="offcanvas" class="btn btn-primary mt-3"> <i class="fa fa-save"></i> Salvar Dados</button>
       <button cancelar type="button" data-bs-dismiss="offcanvas" class="btn btn-danger mt-3"> <i class="fa fa-cancel"></i> Cancelar</button>
 
