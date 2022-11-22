250bd09754999a27355b6104da42762e3b519d5a
livia128924 <livia128924@gmail.com>
2022-11-22 17:05:24 -0300

    atualizando

--- painel/src/componentes/menu_lateral/menu.php
+++ painel/src/componentes/menu_lateral/menu.php
@@ -3,7 +3,7 @@
 ?>
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <img src="img/logopainel.png" style="height:60px;" alt="">
+    <img src="img/logomenup.png" style="height:60px;" alt="">
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
