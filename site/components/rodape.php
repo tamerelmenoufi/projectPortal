5479738621b676303948372444a5119037f94cc2
livia128924 <livia128924@gmail.com>
2022-11-22 20:28:52 -0300

    atualizando

--- site/components/rodape.php
+++ site/components/rodape.php
@@ -7,14 +7,14 @@
 
         <div class="d-flex flex-column align-items-center align-items-lg-start">
           <div class="copyright">
-            &copy; Copyright <strong><span>HeroBiz</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>Project</span></strong>. Todos os direitos reservados
           </div>
           <div class="credits">
             <!-- All the links in the footer should remain intact. -->
             <!-- You can delete the links only if you purchased the pro version. -->
             <!-- Licensing information: https://bootstrapmade.com/license/ -->
             <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+           Desenvolvido por <a href="https://mohatron.com.br/">Mohatron Solucções em TI</a>
           </div>
         </div>
 
