c62293259fb6920285d09d0eaa69b9e52706089f
livia128924 <livia128924@gmail.com>
2022-11-22 18:49:06 -0300

    atualizando

--- site/components/banner_principal.php
+++ site/components/banner_principal.php
@@ -8,14 +8,21 @@ if($_GET['cod']){
   $result = mysqli_query($con, $query);
   $d = mysqli_fetch_object($result);
 ?>
-<section id="" class="">
+
     <div class="col-md-12" data-aos="">
       <img src="<?=$localPainel?>src/volume/banners/<?=$d->imagem?>" class="img-responsive" style="width:100%;margin-top:25px">
-      <h2><?=$d->titulo?> <!-- <span>HeroBiz</span> --></h2>
-      <p><?=$d->descricao?></p>
+      </div>
+      <div class="container" data-aos="fade-up">
+        <div class="section-header">
+          <h2><?=$d->titulo?></h2>
+          <p><?=$d->descricao?></p>
+      
       <div class="d-flex">
         <a href="#about" class="btn-get-started scrollto">Get Started</a>
         <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
       </div>
-    </div>
-  </section>
+      </div>
+     
+
+    
+  
