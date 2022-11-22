b814040816ead14a362c85f81bce5832215869df
livia128924 <livia128924@gmail.com>
2022-11-22 20:21:18 -0300

    atualizando

--- painel/index.php
+++ painel/index.php
@@ -19,7 +19,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="img/icone.png">
-    <title>Project</title>
+    <title>Project - Painel de controle</title>
     <?php
     include("lib/header.php");
     ?>
