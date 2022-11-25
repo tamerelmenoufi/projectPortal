c0558ad1ed1ba70dd453c92403131a8e23b298d2
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-25 09:48:50 -0400

    Atualizações

--- painel/src/portifolio/lista_categorias.php
+++ painel/src/portifolio/lista_categorias.php
@@ -32,7 +32,7 @@
     <div class="row">
       <div class="col">
         <div class="card">
-          <h5 class="card-header">Categoria do Serviço</h5>
+          <h5 class="card-header">Categoria de Serviços</h5>
           <div class="card-body">
             <div style="display:flex; justify-content:end">
                 <button
