1237e002c44a034d457b7fedd4d482fba8fd0794
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-28 14:23:36 -0400

    Atualizações

--- painel/src/configuracoes/index.php
+++ painel/src/configuracoes/index.php
@@ -3,6 +3,14 @@
         <h5 class="card-header">Endereço</h5>
         <div class="card-body">
             Rua fulano de tal, numero, complemento, referencia, bairro, Manaus-AM, CEP
+
+            <div class="mb-3">
+                <label for="inputPassword5" class="form-label">Rua</label>
+                <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+            </div>
+
+
+
         </div>
     </div>
 </div>
