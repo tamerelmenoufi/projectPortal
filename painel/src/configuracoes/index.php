e4a638c33c00de9b4825a7b3856158c073843cef
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-28 17:06:28 -0400

    Atualizações

--- painel/src/configuracoes/index.php
+++ painel/src/configuracoes/index.php
@@ -2,14 +2,47 @@
     <div class="card">
         <h5 class="card-header">Endereço</h5>
         <div class="card-body">
-            Rua fulano de tal, numero, complemento, referencia, bairro, Manaus-AM, CEP
+            <div class="row">
+                <div class="col-md-6">
 
-            <div class="mb-3">
-                <label for="inputPassword5" class="form-label">Rua</label>
-                <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+                    <div class="mb-3">
+                        <label class="form-label">CEP</label>
+                        <div class="form-control"><?=$d->cep?></div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Rua</label>
+                        <div class="form-control" ><?=$d->rua?></div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Número</label>
+                        <div class="form-control" ><?=$d->numero?></div>
+                    </div>
+
+
+                    <div class="mb-3">
+                        <label class="form-label">Bairro</label>
+                        <div class="form-control" ><?=$d->bairro?></div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Complemento</label>
+                        <div class="form-control" ><?=$d->complemento?></div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Referência</label>
+                        <div class="form-control" ><?=$d->referencia?></div>
+                    </div>
+
+                </div>
+
+                <div class="col-md-6">
+                    Lado oposto
+                </div>
+
             </div>
-
-
 
         </div>
     </div>
