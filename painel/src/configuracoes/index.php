67ac1b9671f13565f69322783c47b8bd98f308c6
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-28 14:18:26 -0400

    Atualizações

--- painel/src/configuracoes/index.php
+++ painel/src/configuracoes/index.php
@@ -1,10 +1,16 @@
-Config
+<div class="card">
+    <h5 class="card-header">Endereço</h5>
+    <div class="card-body">
+        Rua fulano de tal, numero, complemento, referencia, bairro, Manaus-AM, CEP
+    </div>
+</div>
 
 
 <script>
     $(function(){
 
         Carregando('none');
+
 
 
     })
