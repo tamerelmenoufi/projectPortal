c747e2e96e1794f320490888216d0b65d323a6aa
livia128924 <livia128924@gmail.com>
2022-12-01 16:16:04 -0300

    atualizando

--- painel/src/componentes/menu_topo/menu.php
+++ painel/src/componentes/menu_topo/menu.php
@@ -12,7 +12,7 @@
   <div class="container-fluid">
     <div data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
      
-    <div  class=".d-none .d-xs-block">
+    <div  class="d-none d-sm-block">
     <img src="img/logomenup.png" style="height:40px; margin-right:20px;" >
 </div>
 
