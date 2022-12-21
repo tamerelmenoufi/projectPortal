7bb70a9010fbd26b7441b51690dc7585f8f61aa6
livia128924 <livia128924@gmail.com>
2022-12-21 12:39:47 -0300

    atualizando

--- site/components/noticias_detalhes.php
+++ site/components/noticias_detalhes.php
@@ -323,7 +323,7 @@ border-radius: 0;
                   </div><!-- End recent post item-->
 
                 </div>
-                <button type="button" class="btn btn-outline-primary">Mais Noticias</button>
+               <center style="margin-top:10px"> <button type="button" class="btn btn-outline-info">Mais Noticias</button></center>
               </div><!-- End sidebar recent posts-->
 
               <!-- <div class="sidebar-item tags">
