731d47cf9a33c0ea2cd6648893145a60e2a060ed
livia128924 <livia128924@gmail.com>
2022-12-21 18:56:35 -0300

    atualizando

--- site/components/noticias.php
+++ site/components/noticias.php
@@ -11,7 +11,7 @@
         <div class="row">
 
           <?php
-          $query = "select * from noticias where situacao = '1' order by data desc limit 0,3 ";
+          $query = "select * from noticias where situacao = '1' order by data asc limit 0,3 ";
           $result = mysqli_query($con, $query);
           while($d = mysqli_fetch_object($result)){
           ?>
