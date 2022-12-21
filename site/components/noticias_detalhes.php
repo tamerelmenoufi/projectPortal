fe0152cc33cd1033ea7f1d1f26eff6f1a003dc59
livia128924 <livia128924@gmail.com>
2022-12-21 09:54:17 -0300

    atualizando

--- site/components/noticias_detalhes.php
+++ site/components/noticias_detalhes.php
@@ -35,7 +35,7 @@
 
         <div class="row g-5">
 
-          <div class="col-lg-12 ">
+          <div class="col-lg-8">
 
             <article class="blog-details clearfix">
 
@@ -86,11 +86,63 @@
                 </p>
 
 
-                <button  type="button"  class="float-end  botaoazul">
+              
+
+              <div class="col-lg-4">
+              <div class="sidebar-item recent-posts">
+                <h3 class="sidebar-title">Recent Posts</h3>
+
+                <div class="mt-3">
+
+                  <div class="post-item mt-3">
+                    <img src="assets/img/blog/blog-recent-1.jpg" alt="" class="flex-shrink-0">
+                    <div>
+                      <h4><a href="blog-post.html">Nihil blanditiis at in nihil autem</a></h4>
+                      <time datetime="2020-01-01">Jan 1, 2020</time>
+                    </div>
+                  </div><!-- End recent post item-->
+
+                  <div class="post-item">
+                    <img src="assets/img/blog/blog-recent-2.jpg" alt="" class="flex-shrink-0">
+                    <div>
+                      <h4><a href="blog-post.html">Quidem autem et impedit</a></h4>
+                      <time datetime="2020-01-01">Jan 1, 2020</time>
+                    </div>
+                  </div><!-- End recent post item-->
+
+                  <div class="post-item">
+                    <img src="assets/img/blog/blog-recent-3.jpg" alt="" class="flex-shrink-0">
+                    <div>
+                      <h4><a href="blog-post.html">Id quia et et ut maxime similique occaecati ut</a></h4>
+                      <time datetime="2020-01-01">Jan 1, 2020</time>
+                    </div>
+                  </div><!-- End recent post item-->
+
+                  <div class="post-item">
+                    <img src="assets/img/blog/blog-recent-4.jpg" alt="" class="flex-shrink-0">
+                    <div>
+                      <h4><a href="blog-post.html">Laborum corporis quo dara net para</a></h4>
+                      <time datetime="2020-01-01">Jan 1, 2020</time>
+                    </div>
+                  </div><!-- End recent post item-->
+
+                  <div class="post-item">
+                    <img src="assets/img/blog/blog-recent-5.jpg" alt="" class="flex-shrink-0">
+                    <div>
+                      <h4><a href="blog-post.html">Et dolores corrupti quae illo quod dolor</a></h4>
+                      <time datetime="2020-01-01">Jan 1, 2020</time>
+                    </div>
+                  </div><!-- End recent post item-->
+
+                </div>
+
+              </div><!-- End sidebar recent posts-->
+ </div>
+
+ <button  type="button"  class="float-end  botaoazul">
                 <a style="color:#fff" href="javascript:history.back()">Voltar</a></button>
               </div><!-- End post content -->
 
-            
 
     </section><!-- End Blog Details Section -->
     </div>
