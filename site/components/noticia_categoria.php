3b8091eee4920bca3eadf3c505d067597dfd2f98
livia128924 <livia128924@gmail.com>
2022-12-21 19:07:02 -0300

    atualizando

--- site/components/noticia_categoria.php
+++ site/components/noticia_categoria.php
@@ -81,19 +81,13 @@ border-radius: 0;
 
 
         <center style="margin-top:15px">
-<nav aria-label="...">
-  <ul style="margin:0px" class="pagination">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
-    </li>
+        <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
     <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active" aria-current="page">
-      <a class="page-link" href="#">2</a>
-    </li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
     <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Proximo</a>
-    </li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
   </ul>
 </nav>
         </center>
