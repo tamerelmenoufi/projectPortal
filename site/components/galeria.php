0607a243e6ad19c4f50703fa04d99e6d147122a3
livia128924 <livia128924@gmail.com>
2022-12-21 13:05:51 -0300

    atualizando

--- site/components/galeria.php
+++ site/components/galeria.php
@@ -43,7 +43,7 @@
               <div class="portfolio-info">
                 <h4><?=$d->titulo?></h4>
                 <a href="<?=$localPainel?>src/volume/portifolio/<?=$d->imagem?>" title="<?=$d->titulo?>" data-gallery="portfolio-gallery" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                <a href="servicos.php?cod=<?=$d->codigo?>" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                <a href="servico.php?cod=<?=$d->codigo?>" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div><!-- End Portfolio Item -->
             <?php
