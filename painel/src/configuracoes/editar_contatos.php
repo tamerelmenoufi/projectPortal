08072983d7073f4aa2b134a1d1e0d7e5a11229a7
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-08-31 18:53:04 +0300

    Atualizações

--- painel/src/configuracoes/editar_contatos.php
+++ painel/src/configuracoes/editar_contatos.php
@@ -1,5 +1,5 @@
 <?php
-    include("{$_SERVER['DOCUMENT_ROOT']}/credcarbon/painel/lib/includes.php");
+    include("{$_SERVER['DOCUMENT_ROOT']}/portal/painel/lib/includes.php");
 
     if($_POST['acao'] == 'contatos'){
 
